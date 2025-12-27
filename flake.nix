6589bd1eefbe2d1dfeaf2ab24f646834e7f3e137
Marcin Skalski <skalskimarcin33@gmail.com>
2025-12-27 10:45:15 +0100

    fix: add backup extension for existing files
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users."marcin.skalski@konghq.com" = import ./modules/home.nix;
           }
         ];
